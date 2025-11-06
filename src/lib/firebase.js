6bda670fc6de7da8a0bf3325fbf84d3d6b90c639
rohaanumair <m.rohaanumair@gmail.com>
2025-11-06 11:44:51 +0500

    updated

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -8,13 +8,13 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, si
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAqtVQ4_8Wu4MFYFhmBpuJvOMIOwCxJ31w",
-    authDomain: "chat-app-d2b05.firebaseapp.com",
-    databaseURL: "https://chat-app-d2b05-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "chat-app-d2b05",
-    storageBucket: "chat-app-d2b05.firebasestorage.app",
-    messagingSenderId: "158221550773",
-    appId: "1:158221550773:web:8a044921a1ded515f7aafc"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
